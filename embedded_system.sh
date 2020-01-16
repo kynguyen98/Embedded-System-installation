82c13f925b5bd39ce7f487564ca9ed856c5f3569
Nguyen Hong Ky <nguyenhongky1998@gmail.com>
2020-01-16 03:56:32 -0800

    Updated script

--- embedded_system.sh
+++ embedded_system.sh
@@ -2,15 +2,20 @@
 # Begin code
 start=$SECONDS
 # Update Manjaro
+echo "Removing all bloatware"
+sudo pacman -Rs hplip thunderbird yakuake skanlite firefox konversation --noconfirm
 sudo pacman -Syu --noconfirm
 
+# Install dependencies
+sudo pacman -S java-environment-common jre8-openjdk jre8-openjdk-headless jdk8-openjdk c-ares electron6 ripgrep jre11-openjdk code linux54-virtualbox-host-modules linux54-headers --noconfirm
+
 echo "Updating completed"
-sleep 3
+
 # Toolchain
 echo "Installing ARM toolchain......"
 sudo pacman -S openocd gdb arm-none-eabi-gcc arm-none-eabi-gdb python-pyserial arm-none-eabi-newlib --noconfirm
 echo "Toolchain completed"
-sleep 3
+
 # IDE
 echo "Installing Embedded System IDE"
 echo "Installing STM32CubeMX"
@@ -18,31 +23,25 @@ git clone https://aur.archlinux.org/stm32cubemx.git
 cd stm32cubemx
 makepkg -si
 cd ..
-m -r stm32cubemx
 echo "STM32CubeMX installed"
-sleep 1
+
 echo "Installing Arduino..."
 sudo pacman -S arduino --noconfirm
 sudo usermod -aG uucp,lock john
-sudo modprobe cdc_acm
+sudo modprobe cdc_acm vboxdrv
 echo "Arduino installed"
-sleep 1
 echo "Installing stlink..."
 sudo pacman -S stlink --noconfirm
 echo "STlink installed"
-sleep 1
 echo "Installing VSCode..."
 sudo pacman -S code --noconfirm
 echo "VSCode installed"
-sleep 1
 echo "Installing SW4STM32..."
 git clone https://aur.archlinux.org/sw4stm32.git
 cd sw4stm32
 makepkg -si
 cd ..
-rm -r sw4stm32
 echo "SW4STM32 installed"
-sleep 1
 echo "Installing Daily stuff..."
 sudo pacman -S qbittorrent wireshark-qt virtualbox mutt neomutt --noconfirm
 
@@ -52,23 +51,23 @@ git clone https://github.com/kynguyen98/Simple-Mutt-config.git
 cd Simple-Mutt-config
 sudo cp color.muttrc mailcap muttrc ~/.config/mutt
 cd ..
-rm -r Simple-Mutt-config
+
 # Daily stuff installing
-sudo pacman -Rs firefox --noconfirm
 git clone https://aur.archlinux.org/google-chrome.git
 cd google-chrome
-makepgk -si
+makepkg -si
 cd ..
-rm -rf google-chrome
+echo "Chrome installed"
 sudo pacman -S gimp --noconfirm
 echo "Daily stuff installed"
-sleep 1
 echo "Installing Octave..."
 sudo pacman -S octave --noconfirm
 echo "Octave installed"
-sleep 1
-echo "Removing all bloatware"
-sudo pacman -Rs hplip thunderbird yakuake skanlite --noconfirm
-
+sudo rm -r stm32cubemx
+sudo rm -r sw4stm32
+sudo rm -r Simple-Mutt-config
+sudo rm -r google-chrome
+echo "cleaning cache"
+sudo pacman -Sc --noconfirm
 echo "All completed it took $((SECONDS - start)) seconds to complete"
  
