5afec6455c5459fea6acad189f378c8b75768cd0
kynguyen98 <nguyenhongky1998@gmail.com>
2020-03-19 22:26:44 +0700

    Updated wizard

--- embedded_system.sh
+++ embedded_system.sh
@@ -2,24 +2,38 @@
 # Begin code
 start=$SECONDS
 
+# Add some pre-define alias
+
+touch .bash_aliases
+echo -e "alias install="sudo pacman -S"\nalias update="sudo pacman -Syu"\nalias remove="sudo pacman -Rs"\nalias search="pacman -Ss"\nalias st="st-flash"">>.bash_aliases
+echo -e "function_update_ps1() {\n    PS1=$(powerline-shell $?)\n}\n">>.bashrc
+echo -e "if [[ $TERM != linux && ! $PROMPT_COMMAND =~ _update_ps1 ]]; then\n    PROMPT_COMMAND="_update_ps1; $PROMPT_COMMAND"\nfi\n">.bashrc
+echo -e "if [ -f ~/.bash_aliases ]; then\n. ~/.bash_aliases\nfi\n">>.bashrc
+
 # Loading kernel module
+
 sudo modprobe cdc_acm vboxdrv
 
 # Adding user to group
+
 sudo usermod -aG uucp,lock john
 
-# Update Manjaro
-sudo pacman -Rs hplip thunderbird yakuake skanlite firefox konversation --noconfirm
-sudo pacman -Syu --noconfirm
+# Update Manjaro with pre-define aliases 
+
+remove -Rs hplip thunderbird yakuake skanlite firefox konversation --noconfirm
+update -Syu --noconfirm
 sudo pip install --upgrade pip
 
 # Install dependencies
-sudo pacman -S java-environment-common jre8-openjdk jre8-openjdk-headless jdk8-openjdk c-ares electron6 ripgrep jre11-openjdk code linux-virtualbox-host-modules linux-headers --noconfirm
+
+install java-environment-common jre8-openjdk jre8-openjdk-headless jdk8-openjdk c-ares electron6 ripgrep jre11-openjdk code linux-virtualbox-host-modules linux-headers --noconfirm
 
 # Toolchain
-sudo pacman -S openocd gdb arm-none-eabi-gcc arm-none-eabi-gdb python-pyserial arm-none-eabi-newlib --noconfirm
+
+install openocd gdb arm-none-eabi-gcc arm-none-eabi-gdb python-pyserial arm-none-eabi-newlib --noconfirm
 
 # Clonning stuff from git 
+
 git clone https://aur.archlinux.org/stm32cubemx.git
 git clone https://aur.archlinux.org/ncurses5-compat-libs.git
 git clone https://github.com/kynguyen98/Simple-Mutt-config.git
@@ -27,6 +41,7 @@ git clone https://aur.archlinux.org/google-chrome.git
 git clone https://github.com/kynguyen98/stm32pio.git
 
 # Start installing 
+
 cd stm32cubemx
 makepkg -si --noconfirm
 cd ..
@@ -35,9 +50,10 @@ cd stm32pio
 sudo pip install .
 cd ..
 
-# Installing platformio core 
-pip install -U platformio
+# Installing pip packages
 
+sudo pip install -U platformio
+sudo pip install pwerline-shell
 cd ncurses5-compat-libs
 makepkg -si --noconfirm
 cd..
@@ -52,12 +68,15 @@ makepkg -si --noconfirm
 cd ..
 
 # Some software that I like to use
+
 sudo pacman -S vim octave arduino gimp stlink code qbittorrent wireshark-qt virtualbox mutt neomutt --noconfirm
+
 # Remove some left over folder 
+
 sudo rm -r -f stm32cubemx
 sudo rm -r -f Simple-Mutt-config
 sudo rm -r -f google-chrome
 sudo pacman -Sc --noconfirm
 echo "All completed it took $((SECONDS - start)) seconds to complete"
  
-##############################################################################################################################
+#####################################################################################################################################################################################################
